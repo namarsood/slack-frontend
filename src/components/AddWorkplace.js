84c81e2be92dec911df5f7c00e0c53a89691aa84
Namardeep Sood <namardeep.sood@bambooapp.com>
2020-10-01 21:25:29 +0530

    add workplace dialog added and handeled using useRef hook

--- src/components/AddWorkplace.js
+++ src/components/AddWorkplace.js
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
  import '../styles/AddWorkplace.css';
 
 function AddWorkplace(props) {
 
-    let nextWorkplace = "w"+ (props.currentWorkplacesCount + 1);
+    let [diplayWorkplaceOptions, changeWorkplaceOptionsVisibility] = useState(()=>"none");
 
-    function addWorkplace(){
-        props.addWorkplace((prevWorkplaces)=>[...prevWorkplaces, {name: nextWorkplace}]);
-      }
+    // let nextWorkplace = "w"+ (props.currentWorkplacesCount + 1);
+
+    // function addWorkplace(){
+    //     props.addWorkplace((prevWorkplaces)=>[...prevWorkplaces, {name: nextWorkplace}]);
+    // }
+
+    function showWorkplaceOptionsDialog(){
+        changeWorkplaceOptionsVisibility((prevState)=> prevState==="none"?"block":"none");
+    }
+
+    const wrapperRef = useRef(null);
+    useOutsideAlerter(wrapperRef, diplayWorkplaceOptions, showWorkplaceOptionsDialog);
 
   return (
     <div className="add-workplace">
-        <button className="add-workplace-btn" onClick={addWorkplace}> 
+        <button className="add-workplace-btn" onClick={showWorkplaceOptionsDialog}> 
             +
         </button>
+        <div className="workplace-options" style={{display: diplayWorkplaceOptions}} ref={wrapperRef}>
+            workplace dialog
+        </div>
     </div>
   );
+}
+
+function useOutsideAlerter(ref, diplayWorkplaceOptions, showWorkplaceOptionsDialog) {
+    useEffect(() => {
+        /**
+         * Alert if clicked on outside of element
+         */
+        function handleClickOutside(event) {
+            console.log("here", diplayWorkplaceOptions);
+            if (ref.current && !ref.current.contains(event.target) && diplayWorkplaceOptions === "block") {
+            
+                showWorkplaceOptionsDialog();
+            }
+        }
+
+        // Bind the event listener
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            // Unbind the event listener on clean up
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [ref, diplayWorkplaceOptions, showWorkplaceOptionsDialog]);
 }
 
 export default AddWorkplace;
